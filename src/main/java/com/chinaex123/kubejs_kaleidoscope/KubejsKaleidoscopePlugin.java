396fb9119784b288a4bbc4934de1d13d48c30c39
EvanHsieh0415 <evan940415@gmail.com>
2026-07-26 17:46:25 +0800

    Remove Kaleidoscope Cookery's required dependence (KubeJS support builtin in original mod)

--- src/main/java/com/chinaex123/kubejs_kaleidoscope/KubejsKaleidoscopePlugin.java
+++ src/main/java/com/chinaex123/kubejs_kaleidoscope/KubejsKaleidoscopePlugin.java
@@ -27,13 +27,6 @@ public class KubejsKaleidoscopePlugin implements KubeJSPlugin {
 
     @Override
     public void registerRecipeSchemas(RecipeSchemaRegistry registry) {
-        registry.register(ResourceLocation.parse("kaleidoscope_cookery:stockpot"), StockpotRecipeJS.SCHEMA); // 森罗物语：厨房 - 汤锅配方
-        registry.register(ResourceLocation.parse("kaleidoscope_cookery:pot"), PotRecipeJS.SCHEMA); // 森罗物语：厨房 - 炒锅配方
-        registry.register(ResourceLocation.parse("kaleidoscope_cookery:chopping_board"), ChoppingBoardRecipeJS.SCHEMA); // 森罗物语：厨房 - 菜板配方
-        registry.register(ResourceLocation.parse("kaleidoscope_cookery:millstone"), MillstoneRecipeJS.SCHEMA); // 森罗物语：厨房 - 石磨配方
-        registry.register(ResourceLocation.parse("kaleidoscope_cookery:steamer"), SteamerRecipeJS.SCHEMA); // 森罗物语：厨房 - 蒸笼配方
-        registry.register(ResourceLocation.parse("kaleidoscope_cookery:teapot"), TeapotRecipeJS.SCHEMA); // 森罗物语：厨房 - 茶壶配方
-
         if (ModList.get().isLoaded("kaleidoscope_tavern")) {
             registry.register(ResourceLocation.parse("kaleidoscope_tavern:pressing_tub"), PressingTubRecipeJS.SCHEMA); // 森罗物语：酒馆 - 果盆配方
             registry.register(ResourceLocation.parse("kaleidoscope_tavern:barrel"), BarrelRecipeJS.SCHEMA); // 森罗物语：酒馆 - 酒桶配方
